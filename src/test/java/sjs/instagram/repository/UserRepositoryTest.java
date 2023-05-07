f19e6dda45abbc115b452be05b6d2dca7b004fd6
sucinicacid <sucinicacid2002@gmail.com>
2023-05-07 17:00:58 +0900

    Fix UserRepositoryTest

--- src/test/java/sjs/instagram/repository/UserRepositoryTest.java
+++ src/test/java/sjs/instagram/repository/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import sjs.instagram.TestDataFactory;
 import sjs.instagram.domain.User;
 import sjs.instagram.domain.UserInfo;
@@ -19,11 +20,14 @@ class UserRepositoryTest {
 
     @Autowired TestDataFactory testDataFactory;
     @Autowired UserRepository userRepository;
+    @Autowired PasswordEncoder passwordEncoder;
 
     @Test
     void 사용자_생성() {
-        UserInfo userInfo = new UserInfo("username", "photo", "instagram_id", "introduction");
+        UserInfo userInfo = new UserInfo("name", "photo", "instagram_id", "introduction");
         User user = new User(userInfo);
+        user.setUsername("username");
+        user.setPassword(passwordEncoder.encode("password"));
 
         User saved = userRepository.save(user);
 
@@ -39,19 +43,19 @@ class UserRepositoryTest {
         List<User> findAll = userRepository.findAll();
 
         assertThat(find).isEqualTo(user1);
-        assertThat(findAll.size()).isEqualTo(2);
+        assertThat(findAll.size()).isEqualTo(3);
     }
 
     @Test
     void 사용자_수정() {
         User user = testDataFactory.createUser();
-        UserInfo userInfo = new UserInfo("new username", "new photo", "new instagram_id", "new introduction");
+        UserInfo userInfo = new UserInfo("new name", "new photo", "new instagram_id", "new introduction");
         user.changeUserInfo(userInfo);
 
         User updated = userRepository.save(user);
 
         UserInfo updatedUserInfo = updated.getUserInfo();
-        assertThat(updatedUserInfo.getName()).isEqualTo("new username");
+        assertThat(updatedUserInfo.getName()).isEqualTo("new name");
         assertThat(updatedUserInfo.getPhoto()).isEqualTo("new photo");
         assertThat(updatedUserInfo.getInstagram_id()).isEqualTo("new instagram_id");
         assertThat(updatedUserInfo.getIntroduction()).isEqualTo("new introduction");
@@ -64,6 +68,6 @@ class UserRepositoryTest {
         userRepository.delete(user);
 
         long count = userRepository.count();
-        assertThat(count).isEqualTo(0);
+        assertThat(count).isEqualTo(1);
     }
 }
